4687c044331cc3c68a871db512da150a481ab8f4
gigihpaga <gigihpaga@gmail.com>
2023-03-17 18:22:45 +0700

    create section: Hero and About ✨

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,20 @@ module.exports = {
     content: ['./src/**/*.{html,js}', 'index.html'],
     mode: 'jit',
     theme: {
-        extend: {},
+        container: {
+            center: true,
+            padding: '16px',
+        },
+        extend: {
+            colors: {
+                primary: '#14b8a6',
+                secondary: '#64748b',
+                dark: '#0f172a',
+            },
+            screens: {
+                '2xl': '1320px',
+            },
+        },
     },
     plugins: [],
 };
-
